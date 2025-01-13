bcdd966307436f39a897cd39bf1c6c5119c7ccdc
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-13 22:51:38 +1100

    created new print format

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str3 = str1 + " "
-print(f"Welcome to {str3 + str2}!")
+str1 += " " # apending a space to str1
+print(f"Welcome to {str1 + str2}!")
