4839c91ae587564bee0a46dc41ea047916f33f80
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-18 17:49:36 +1100

    updated permissions for file

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    str_frame = ""
-    for i in str:
-            if ord(i) >= 96 and ord(i) <= 123:
-                  str_frame += chr(ord(i) - 32)
-            print("{}".format(str_frame), end="")
+    for c in str:
+            if ord(c) >= 97 and ord(c) <= 122:
+                  c = chr(ord(c) - 32)
+            print("{}".format(c), end="")
     print("")
