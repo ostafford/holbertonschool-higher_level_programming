882856a39e3c6d42f402985a7969d2260c98ef76
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-18 17:24:41 +1100

    udpated n to numbers name

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for n in range(0, 100):
-    if n == 99:
-        print("{}".format(n))
+for numbers in range(0, 100):
+    if numbers == 99:
+        print("{}".format(numbers))
     else:
-        print("{:02}".format(n), end=", ")
+        print("{:02}".format(numbers), end=", ")
