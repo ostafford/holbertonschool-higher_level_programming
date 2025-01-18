ad8b73ffe8a68fbd69db6ff3876052d1aff2d5c1
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-18 17:40:32 +1100

    updated permissions for file

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i >= j:
+for digit_1 in range(0, 10):
+    for digit_2 in range(0, 10):
+        if digit_1 >= digit_2:
             continue
-        if i * j != 72:
-            print("{}{}".format(i, j), end=", ")
+        if digit_1 * digit_2 != 72:
+            print("{}{}".format(digit_1, digit_2))
         else:
-            print("{}{}".format(i, j))
+            print("{}{}".format(digit_1, digit_2), end=", ")
