cac578bfdc4984366434cb55eab75c68c4af8cdf
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-18 17:21:27 +1100

    updated code style

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i != 99:
-        print("{:02}".format(i), end=', ')
+for n in range(0, 100):
+    if n == 99:
+        print("{:02}".format(n), end=", ")
     else:
-        print("{}".format(i))
+        print("{}".format(n))
+        
