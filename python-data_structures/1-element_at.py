cc7b4f4ad0fe375ca62bdc3f78e8179977f57b85
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-20 20:09:31 +1100

    updated if function

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if my_list[idx] < 0 or my_list[idx] > 4:
+    if idx < 0 or idx >= len(my_list):
         return None
     return my_list[idx]
