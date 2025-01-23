30b1e3ac7b56010adbdd71bf2b7759e599ae8765
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-24 08:59:27 +1100

    moved end="" paramater to resolve checker error

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
             if i < len(row) - 1:
                 print("{:d}".format(num), end=" ")
             else:
-                print("{:d}".format(num))
-    print(end="")
+                print("{:d}".format(num), end="")
+    print()
