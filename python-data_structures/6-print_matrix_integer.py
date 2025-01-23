3c8ac9cefe9474df4fbf482e24b27480128828c9
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-01-24 09:38:01 +1100

    removed last end=""

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,6 @@ def print_matrix_integer(matrix=[[]]):
             if i < len(row) - 1:
                 print("{:d}".format(num), end=" ")
             else:
-                print("{:d}".format(num))
+                print("{:d}".format(num), end="")
+                
     print()
