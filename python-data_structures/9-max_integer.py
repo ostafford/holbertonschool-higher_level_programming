d5415b76af7a9ed95a54f18edb699c39ce203de0
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-01 22:45:35 +1100

    created file 10

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -8,3 +8,10 @@ def max_integer(my_list=[]):
         if num > max_num:
             max_num = num
     return max_num
+
+# check if list is empty - return NONE
+# Set starting point of list (max_num)
+# Start the iteration through the list of numbers with for loop
+# set the condition of if iteration (num) is bigger than the starting point (max_num)
+# set max_num to num
+# return the highest value. 
