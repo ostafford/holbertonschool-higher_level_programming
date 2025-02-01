d920598097fc3edd69c72864fcb9412ff946906b
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-01 23:48:15 +1100

    created file 11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     try:
-        del my_list[idx]
+        return my_list[:idx] + my_list[idx+1:]
     except (IndexError, TypeError):
-        pass
-    return my_list
+        return my_list.copy()
