00fdae09ae93857364f9a249bd8272b5819be4e0
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-02 22:29:51 +1100

    created file 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for name printing.
+Contains a function that prints a full name.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints 'My name is <first_name> <last_name>'.
+
+    Args:
+        first_name (str): The first name
+        last_name (str): The last name, defaults to empty string
+
+    Raises:
+        TypeError: If first_name or last_name are not strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
+    
