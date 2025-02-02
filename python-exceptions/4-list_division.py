66e6df5f5698e683de28de3b8a7cc9b0a1335dcb
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-02 19:03:04 +1100

    updated code style

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,8 +9,8 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             div = 0
             if i < len(my_list_1) and i < len(my_list_2):
-                if not (isinstance(my_list_1[i], (int, float)) and 
-                       isinstance(my_list_2[i], (int, float))):
+                if not (isinstance(my_list_1[i], (int, float)) and
+                        isinstance(my_list_2[i], (int, float))):
                     print("wrong type")
                 elif my_list_2[i] == 0:
                     print("division by 0")
