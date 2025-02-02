f96258d6c3cbe0bfebebba02541b036b7d5bd48a
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-02 18:50:51 +1100

    created file 1

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    pass
+    """
+    Prints the first x elements of a list and only integers
+    Returns the real number of integers printed
+    """
+    count = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end="")
+                count += 1
+        print()
+        return count
+    except IndexError:
+        raise
