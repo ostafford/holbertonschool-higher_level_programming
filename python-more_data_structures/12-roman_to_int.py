ef4db1c1450df5001c59d369b7219387f9abc493
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-02 17:11:36 +1100

    resolved pycodestyle error

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,15 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    roman_values = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+        }
     total = 0
     for i in range(len(roman_string)):
         current = roman_values[roman_string[i]]
