38b10d5eb3ddfff750519d6b70a98b19ee3bb33e
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-02 17:05:45 +1100

    created file 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_values = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    total = 0
+    for i in range(len(roman_string)):
+        current = roman_values[roman_string[i]]
+        if i < len(roman_string) - 1:
+            next_val = roman_values[roman_string[i+1]]
+            if current < next_val:
+                total -= current
+            else:
+                total += current
+        else:
+            total += current
+    return total
