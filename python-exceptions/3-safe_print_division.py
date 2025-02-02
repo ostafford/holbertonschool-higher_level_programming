956aafcd67757a13209f0cf91da2002ba9c162c6
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-02 18:59:13 +1100

    created file 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    pass
+    """
+    Divides 2 integers and prints the result
+    Returns the value of the division, otherwise: None
+    """
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+        return result
