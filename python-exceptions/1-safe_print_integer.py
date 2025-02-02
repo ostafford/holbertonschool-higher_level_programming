213034e62f46989f02ef6b823df8711da9a015c6
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-02 18:57:10 +1100

    created file 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,8 @@
 def safe_print_integer(value):
     """
     Prints an integer with "{:d}".format()
-    Returns True if value has been correctly printed (it means the value is an integer)
+    Returns True if value has been
+    correctly printed (it means the value is an integer)
     Otherwise returns False
     """
     try:
