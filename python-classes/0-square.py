f144d93fd50de921f7c2cb92245309755533c9de
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-03 14:41:11 +1100

    created file 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+class Square:
+    """Empty class that defines a square.
+
+    This class is a basic implementation
+    of a square with no attributes or methods.
+    """
+    pass
