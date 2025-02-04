53c45a6bcd23c70556efc418fcd670f4462a8dc5
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-04 12:30:57 +1100

    created file 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Define base geometry class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''base geometry'''
+
+    def area(self):
+        '''Exception: Not implemented'''
+        raise Exception("area() is not implemented")
