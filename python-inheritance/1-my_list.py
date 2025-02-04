a59d8ebb68ee9c9a376e4a1860553cf2165cb0b9
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-04 15:02:36 +1100

    Updated test.txt files

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 '''inherit list from class MyList'''
 
+
 class MyList(list):
     '''sort printing for the built-in list class'''
-
     def print_sorted(self):
         print(sorted(self))
