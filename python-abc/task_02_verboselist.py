7f1e3d4cf8847a2dd568641f94482790d1536320
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-04 17:56:09 +1100

    create file 02

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,17 @@
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, items):
+        super().extend(items)
+        print(f"Extended the list with [{len(items)}] items.")
+
+    def remove(self, item):
+        super().remove(item)
+        print(f"Removed [{item}] from the list.")
+
+    def pop(self, index=-1):
+        item = super().pop(index)
+        print(f"Popped [{item}] from the list.")
+        return item
