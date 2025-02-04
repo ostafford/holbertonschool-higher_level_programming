529b63091486dea903759541d0a54837ad2f1dfa
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-04 11:24:05 +1100

    updated pycode style

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''class-checking function'''
 
+
 def is_same_class(obj, a_class):
     '''Check if object is TRUE for instance or given class'''
-    
-    if type(obj) == a_class:
+    if type(obj) is a_class:
         return True
     return False
