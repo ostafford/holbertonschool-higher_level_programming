0b0699c0e038bd6f8fcc7f990b70d09e6858f247
Okky Stafford <142310611+ostafford@users.noreply.github.com>
2025-02-04 14:38:16 +1100

    created file 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''Defines class rectangle that inherits from BaseGeo'''
+'''Defines class rectangle that inherits from BaseGeometry'''
 BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    '''represents rectangle using BaseGeo'''
+    '''represents rectangle using BaseGeometry'''
 
     def __init__(self, width, height):
-        '''Method for new Rectangle'''
+        '''initialize for new Rectangle'''
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
