3b8b84adb2cf86aa7792fb89090b170c03150866
Okky <okky.mstafford@gmail.com>
2025-02-10 18:01:32 +1100

    added Module Documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Reads and prints the content of a
+specified file in UTF-8 encoding.
+This function takes a filename as input,
+opens the file in read mode with UTF-8 encoding,
+reads the entire content of the file,
+and prints it to the standard output.
+"""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, 'r', encoding='UTF-8') as f:
-        content = f.read()
-    print(content)
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read(), end="")
