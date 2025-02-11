179b3872e8644cdfcc79c9d7ba47144660f23917
Okky <okky.mstafford@gmail.com>
2025-02-12 06:59:24 +1100

    Updated Pycodestyle

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,6 +4,7 @@ the JSON representation of an object (string):'''
 
 import json
 
+
 def to_json_string(my_obj):
     '''returns the JSON representation of an object'''
     return json.dumps(my_obj)
