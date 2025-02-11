b8187935a52e455765e532d1e12e153add02623f
Okky <okky.mstafford@gmail.com>
2025-02-12 09:58:24 +1100

    updated code to address checker error

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     '''writes an Object to a text file'''
     with open(filename, 'w', encoding='utf-8') as f:
-        json.dumps(my_obj,)
+        json.dump(my_obj, f)
