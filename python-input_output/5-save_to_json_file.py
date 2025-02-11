55663301d7679fcc699dad4a07e0013b40ca0fc4
Okky <okky.mstafford@gmail.com>
2025-02-12 09:53:26 +1100

    Created files for task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Write a function that writes an Object
+to a text file, using a JSON representation:'''
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dumps(my_obj,)
