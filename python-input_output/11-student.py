47aff8ad6f5e8949bb42dcdf6838ab94b5f6cf53
Okky <okky.mstafford@gmail.com>
2025-02-15 10:15:02 +1100

    Pycodestyle udpate

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -1,24 +1,41 @@
 #!/usr/bin/python3
-'''Write a class Student that
-defines a student by: (based on 10-student.py)'''
+"""Defines a class Student."""
 
 
 class Student:
-    """Class that defines a student."""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize student with first name, last name and age."""
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves dictionary representation of Student instance."""
-        if attrs is None:
-            return self.__dict__
-        return {k: v for k, v in self.__dict__.items() if k in attrs}
+        """Get a dictionary representation of the Student.
+
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance."""
-        for key, value in json.items():
-            setattr(self, key, value)
+        """Replace all attributes of the Student.
+
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-'''Create a function def pascal_triangle(n):
-that returns a list of lists of integers
-representing the Pascal’s triangle of n:'''
+"""Defines a Pascal's Triangle function."""
 
 
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing Pascal's triangle of n."""
+    """Represent Pascal's Triangle of size n.
+
+    Returns a list of lists of
+    integers representing the triangle.
+    """
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)
-        triangle.append(row)
-
-    return triangle
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles
