c0fbea0d997f2ab3ca42a57632116df5512c2451
Okky <okky.mstafford@gmail.com>
2025-02-15 10:09:53 +1100

    Updated

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-
-'''Write a function that returns the dictionary description
-with simple data structure
-(list, dictionary, string, integer and boolean)
-for JSON serialization of an object:'''
+"""Defines a Python class-to-JSON function."""
 
 
-argv = __import__('sys').argv
-exists = __import__('os.path').path.exists
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-filename = "add_item.json"
-if exists(filename):
-    items = load_from_json_file(filename)
-else:
-    items = []
-items.extend(argv[1:])
-save_to_json_file(items, filename)
+def class_to_json(obj):
+    """Return the dictionary representation of a simple data structure."""
+    return obj.__dict__
