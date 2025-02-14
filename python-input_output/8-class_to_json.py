f525248ce1a8e487d073ac66572d493b1825245c
Okky <okky.mstafford@gmail.com>
2025-02-15 10:07:30 +1100

    Updated

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -7,7 +7,7 @@ for JSON serialization of an object:'''
 
 
 argv = __import__('sys').argv
-from os.path import exists
+exists = __import__('os.path').path.exists
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
