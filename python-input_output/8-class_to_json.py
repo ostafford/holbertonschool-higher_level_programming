86f81fa195355847487223a7220aa3abdc0a52f8
Okky <okky.mstafford@gmail.com>
2025-02-15 10:06:03 +1100

    Removed import function

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 '''Write a function that returns the dictionary description
 with simple data structure
 (list, dictionary, string, integer and boolean)
 for JSON serialization of an object:'''
+
 
 argv = __import__('sys').argv
 from os.path import exists
@@ -10,12 +12,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-
 if exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
-
 items.extend(argv[1:])
-
 save_to_json_file(items, filename)
