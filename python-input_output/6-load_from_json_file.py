d3d084d59670b64967121cf01fd619471ed68220
Okky <okky.mstafford@gmail.com>
2025-02-14 11:04:21 +1100

    Update code style

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,6 +4,7 @@ an Object from a “JSON file”:'''
 
 import json
 
+
 def load_from_json_file(filename):
     '''creates an Object from a “JSON file”'''
     with open(filename, 'r', encoding='utf-8') as f:
