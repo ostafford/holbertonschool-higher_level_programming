7d97cecbcba87e36a3a6ddc1021bf8365abe48a7
Okky <okky.mstafford@gmail.com>
2025-03-10 16:01:12 +1100

    Created file 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Full Creation
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+)
+INSERT INTO second_table 
+(id, name, score) VALUES (1, 'John', 10),
+(id, name, score) VALUES (2, 'Alex', 3),
+(id, name, score) VALUES (3, 'Bob', 14),
+(id, name, score) VALUES (4, 'George', 8);
